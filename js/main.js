53c42a903be6b6ab91949f4e368feb360fe74098
HzCode <yirsishertz02@gmail.com>
2020-12-14 15:27:04 -0600

    PWA Format

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function run(){
+	let simbolos = '0123456789abcdef'
+	let color = '#'
+
+	for(let i = 0; i < 6; i++)
+		color += simbolos[Math.floor(Math.random() * 16)]
+
+	document.body.style.background = color
+
+	let texto = document.getElementById("hex")
+	let btn_element = document.getElementById("btn-color")
+
+	texto.innerHTML = color
+	texto.style.color = 'snow'
+	texto.style.textShadow = "2px 0 0 #000, -2px 0 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000"
+
+
+	btn_element.style.color = "snow"
+	btn_element.style.textShadow = "2px 0 0 #000, -2px 0 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000"
+
+}
